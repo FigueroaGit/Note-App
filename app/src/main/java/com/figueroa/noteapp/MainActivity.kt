baa65b6f7c0019f217fa2b12ec1592d04b4d490b
FigueroaGit <hmfp24@gmail.com>
2023-08-25 11:24:54 -0600

    Update libraries and added viewModel

--- app/src/main/java/com/figueroa/noteapp/MainActivity.kt
+++ app/src/main/java/com/figueroa/noteapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.figueroa.noteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,9 +13,12 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.figueroa.noteapp.data.NotesDataSource
 import com.figueroa.noteapp.model.Note
 import com.figueroa.noteapp.screen.NoteScreen
+import com.figueroa.noteapp.screen.NoteViewModel
 import com.figueroa.noteapp.ui.theme.NoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,18 +28,22 @@ class MainActivity : ComponentActivity() {
             NoteAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    val notes = remember {
-                        mutableStateListOf<Note>()
-                    }
-                    NoteScreen(
-                        notes = notes,
-                        onAddNote = { notes.add(it) },
-                        onRemoveNote = { notes.remove(it) }
-                    )
+                    val noteViewModel: NoteViewModel by viewModels()
+                    NotesApp(noteViewModel)
                 }
             }
         }
     }
+}
+
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+    val notesList = noteViewModel.getAllNotes()
+    NoteScreen(
+        notes = notesList,
+        onAddNote = { noteViewModel.addNote(it) },
+        onRemoveNote = { noteViewModel.removeNote(it) }
+    )
 }
 
 @Preview(showBackground = true)
